5e4d706aaa4a173e1867bf1948ea9936dd1b9e27
srquike <srquike@outlook.com>
2022-11-28 15:26:21 -0600

    Se agregó la interfaz para el servicio de los instructivos

---
+++ RegistroTramitesOplagestTrifinio.Services/Interfaces/IInstructivosService.cs
@@ -0,0 +1,18 @@
+﻿using RegistroTramitesOplagestTrifinio.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RegistroTramitesOplagestTrifinio.Services.Interfaces
+{
+    internal interface IInstructivosService
+    {
+        void Create(InstructivoModel instructivo);
+        void Delete(int instructivoId);
+        void Update(InstructivoModel instructivo);
+        List<InstructivoModel> GetInstructivos();
+        InstructivoModel GetInstructivo(int instructivoId);
+    }
+}
