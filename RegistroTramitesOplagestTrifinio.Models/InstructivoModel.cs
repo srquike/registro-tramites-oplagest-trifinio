f5a395600e2fc218e1149580d6cc3043a048f0ba
srquike <srquike@outlook.com>
2022-11-28 15:20:58 -0600

    Se cambio el nombre al modelo Instructivo por InstructivoModel

---
+++ RegistroTramitesOplagestTrifinio.Models/InstructivoModel.cs
@@ -0,0 +1,20 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace RegistroTramitesOplagestTrifinio.Models
+{
+    public class InstructivoModel
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string InstructivoId { get; set; } = string.Empty;
+
+        [BsonElement("numero")]
+        [BsonRepresentation(BsonType.Int32)]
+        public int Numero { get; set; }
+
+        [BsonElement("nombre")]
+        [BsonRepresentation(BsonType.String)]
+        public string Nombre { get; set; } = string.Empty;
+    }
+}
