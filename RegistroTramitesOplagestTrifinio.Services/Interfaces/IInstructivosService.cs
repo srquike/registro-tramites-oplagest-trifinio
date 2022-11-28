561fe3494828ffb4dd14e37dab4c32c8585b7b6b
srquike <srquike@outlook.com>
2022-11-28 16:51:22 -0600

    Se modificó la interfaz del servicio de Instructivos

--- RegistroTramitesOplagestTrifinio.Services/Interfaces/IInstructivosService.cs
+++ RegistroTramitesOplagestTrifinio.Services/Interfaces/IInstructivosService.cs
@@ -5,9 +5,9 @@ namespace RegistroTramitesOplagestTrifinio.Services.Interfaces
     public interface IInstructivosService
     {
         Task Create(InstructivoModel instructivo);
-        Task Delete(int instructivoId);
+        Task Delete(string instructivoId);
         Task Update(InstructivoModel instructivo);
         Task<List<InstructivoModel>> GetInstructivos();
-        Task<InstructivoModel> GetInstructivo(int instructivoId);
+        Task<InstructivoModel> GetInstructivo(string instructivoId);
     }
 }
