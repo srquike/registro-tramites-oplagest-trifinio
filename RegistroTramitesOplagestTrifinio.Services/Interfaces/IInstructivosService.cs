7ee7ee664838139f30bd3dacc176024a659c5b7a
srquike <srquike@outlook.com>
2022-11-28 15:27:57 -0600

    Se quitaron las referencias no utlizadas

--- RegistroTramitesOplagestTrifinio.Services/Interfaces/IInstructivosService.cs
+++ RegistroTramitesOplagestTrifinio.Services/Interfaces/IInstructivosService.cs
@@ -1,9 +1,4 @@
 ﻿using RegistroTramitesOplagestTrifinio.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace RegistroTramitesOplagestTrifinio.Services.Interfaces
 {
