67d34090574a8af3187d803a4a6d3b83f93aee8f
srquike <srquike@outlook.com>
2022-12-07 11:26:04 -0600

    Se creo el modelo para la tabla TramitesRequisitos

---
+++ RegistroTramitesOplagestTrifinio.Models/TramiteRequisitoModel.cs
@@ -0,0 +1,13 @@
+﻿namespace RegistroTramitesOplagestTrifinio.Models
+{
+    public class TramiteRequisitoModel
+    {
+        public int? TramiteRequisitoId { get; set; }
+        public bool? Entregado { get; set; }
+        public bool? Adicional { get; set; }
+        public int? TramiteId { get; set; }
+        public int? RequistoId { get; set; }
+        public TramiteModel? Tramite { get; set; }
+        public RequisitoModel? Requisito { get; set; }
+    }
+}
