4c2a4c0a11897cd460d94d6f73ec452b33bb9e56
srquike <srquike@outlook.com>
2022-12-07 09:48:37 -0600

    Se agrego la implementacion de la interfaz ITramiteService

---
+++ RegistroTramitesOplagestTrifinio.Services/TramitesService.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RegistroTramitesOplagestTrifinio.Data;
+using RegistroTramitesOplagestTrifinio.Models;
+using RegistroTramitesOplagestTrifinio.Services.Interfaces;
+
+namespace RegistroTramitesOplagestTrifinio.Services
+{
+    public class TramitesService : ITramitesService
+    {
+        private readonly ApplicationDbContext _context;
+
+        public TramitesService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<int> Create(TramiteModel tramite)
+        {
+            await _context.AddAsync(tramite);
+            return await _context.SaveChangesAsync();
+        }
+
+        public async Task<int> Delete(int tramiteId)
+        {
+            var resultado = 0;
+
+            if (await GetTramite(tramiteId) is var tramite)
+            {
+                _context.Remove(tramite);
+                resultado = await _context.SaveChangesAsync();
+            }
+
+            return resultado;
+        }
+
+        public async Task<TramiteModel> GetTramite(int tramiteId)
+        {
+            return await _context.Tramites.AsNoTracking().FirstOrDefaultAsync(t => t.TramiteId.Equals(tramiteId));
+        }
+
+        public async Task<List<TramiteModel>> GetTramites()
+        {
+            return await _context.Tramites.AsNoTracking().ToListAsync();
+        }
+
+        public async Task<int> Update(int tramiteId, TramiteModel tramite)
+        {
+            var resultado = 0;
+
+            if (await GetTramite(tramiteId) is var busqueda)
+            {
+                _context.Tramites.Entry(busqueda).State = EntityState.Modified;
+                resultado = await _context.SaveChangesAsync();
+            }
+
+            return resultado;
+        }
+    }
+}
