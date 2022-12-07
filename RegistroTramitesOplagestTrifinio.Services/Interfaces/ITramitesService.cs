668836bae41b9a5143096e6ad1f7164db2a09161
srquike <srquike@outlook.com>
2022-12-07 09:16:52 -0600

    Se creo la interfaz para el servicio de los tramites

---
+++ RegistroTramitesOplagestTrifinio.Services/Interfaces/ITramitesService.cs
@@ -0,0 +1,13 @@
+﻿using RegistroTramitesOplagestTrifinio.Models;
+
+namespace RegistroTramitesOplagestTrifinio.Services.Interfaces
+{
+    public interface ITramitesService
+    {
+        Task<int> Create(TramiteModel tramite);
+        Task<int> Delete(string tramiteId);
+        Task<int> Update(TramiteModel tramite);
+        Task<List<TramiteModel>> GetTramites();
+        Task<TramiteModel> GetTramite(string tramiteId);
+    }
+}
