0b5135ea28133ff3325bec07e276568ec7ee7986
srquike <srquike@outlook.com>
2022-12-13 10:25:22 -0600

    Cambios varios

---
+++ RegistroTramitesOplagestTrifinio.Data/Migrations/20221212231425_AddFieldExpedienteToTableTramites.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RegistroTramitesOplagestTrifinio.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddFieldExpedienteToTableTramites : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "expediente",
+                table: "tramites",
+                type: "character varying",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "expediente",
+                table: "tramites");
+        }
+    }
+}
